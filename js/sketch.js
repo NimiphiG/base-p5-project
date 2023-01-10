7942e7587f6600b4fccb7748215ed340d7bcb7ab
NimiphiG <115160805+NimiphiG@users.noreply.github.com>
2023-01-10 11:56:36 +0000

    colour abd animation

--- js/sketch.js
+++ js/sketch.js
@@ -1,20 +1,40 @@
+let x = 0;
+let y = 0;
+let theta = 0;
+let inc = 0.1; // the amount to increment the offset
+let offset = 0; // the amount offset from wave start
+let amplitude;
+let frequency;
+let maxAngle;
+
 function setup() {
+    amplitude = windowHeight / 8;
+    frequency = windowWidth / 8;
+    maxAngle = (windowWidth / frequency) * TAU;
     createCanvas(windowWidth, windowHeight);
     
-    let x = 0;
-    let y = 0;
-    let theta = 0;
-    let amplitude = windowHeight/8;
-    let frequency = windowWidth/8;
-    let maxAngle = (windowWidth/frequency) * TAU
-    while (theta < maxAngle) {
-        y = sin(theta) * amplitude;
-        ellipse(x, y + height*0.5,16);
-        ellipse(x + frequency/2, y + 50 + height*0.5,20);
+}
+
+function draw() {
+    background(0);
+    strokeWeight(5);
+    fill(255,0,0);
+
+    while (theta < maxAngle + offset) {
+        //sin wave
+        y = sin(theta-0.4) * amplitude;
+        fill(255,0,50)
+        ellipse(x + frequency/2, y + height/0.5, 20,50);
+        x = ((theta-offset) / maxAngle) * windowWidth;
+        
+
+        y = cos(theta) * amplitude;
+        fill(255,0,0)
+        ellipse(x + frequency / 2, y + height * 0.5, 20,50);
         theta += 0.1;
-        x = (theta / maxAngle)*windowWidth;
+        x = ((theta-offset) / maxAngle) * windowWidth;
 
     }
-}
-function draw() {
+    offset += inc;
+    theta = offset;
 }
