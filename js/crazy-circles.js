a67dcaf9079528f69c216d67f4e76880ae1adde1
NimiphiG <115160805+NimiphiG@users.noreply.github.com>
2023-01-13 19:27:40 +0000

    sunset x

--- js/crazy-circles.js
+++ js/crazy-circles.js
@@ -1,41 +1,44 @@
 let x = 0;
 let y = 0;
 let theta = 0;
-let inc = 0.01; // the amount to increment the offset
-let offset = 0; // the amount offset from wave start
+let inc = 0.05; // the amount to increment the offset
+let offset = 1.57; // the amount offset from wave start
 let amplitude;
 let frequency;
 let maxAngle;
 let a = 0;
-let clickX = 500 ;
-let clickY = 500;
+let b = 0;
+let length = 10;
+let fr = 10;
 
 
 function setup() {
-   
-    amplitude = windowHeight *8 ;
-    frequency = windowWidth *8 ;
-    maxAngle = (0.05) * TAU;
-    frameRate(50)
-    createCanvas(windowWidth, windowHeight);
 
- window.addEventListener('mousedown',randomClick)
-    function randomClick(){
-        theta = 0
-        x=0
-        a= random(-1,1)
-        console.log(a)
-        clickX = mouseX
-        clickY = mouseY
+    // amplitude = windowHeight * 8;
+    // frequency = windowWidth * 8;
+    maxAngle = (0.05) * TAU;
+    
+    createCanvas(windowWidth, windowHeight);
+    centerX = windowWidth / 2;
+    centerY = windowHeight / 2;
+
+    window.addEventListener('mousedown', mouseDown)
+    function mouseDown() {
+        fr = 100
     }
+    window.addEventListener('mouseup', mouseUp)
+    function mouseUp() {
+        fr = 10
+    }
+    
 }
 function draw() {
     background(0);
     noStroke();
-    let length = 5;
+    
 
     while (theta < maxAngle + offset) {
-            amplitude = sin(theta-offset)
+        // amplitude = sin(theta - offset)
         //     // amplitude = ((theta-offset)/maxAngle)* (windowHeight/2)
 
         //     //for mouse move
@@ -47,19 +50,19 @@ function draw() {
         //     noFill()
         //     ellipse(x+windowWidth/2, y+windowHeight/2,500,500);
         //     }
-
+frameRate(fr)
 
         // //for click
         for (let i = length; i > 0; i--) {
-            y = (random( 200)*a) + ( random(200)*a)
-            x = (random( 200)*a) + ( random(200)*a);
-            stroke(255 - (250 / length * i ), 60, 60)
+            a = cos(theta) * 2
+            y = (random(200) * a) + (random(200) * a) 
+            x = (random(200) * a) + (random(200) * a);
+            stroke(225 - (255 / length * i), 60, 50,225)
             strokeWeight(10*i)
-            noFill()
-           ellipse(x + clickX, y + clickY, sin(theta)*400*i, sin(theta)*400*i);
+            fill(200,100, 0,60)
+            ellipse((x) + centerX , centerY, sin(theta) * 500 *(i/4), (sin(theta)) * 500 *(i/4));
 
         }
-
         theta += 0.5;
         x = ((theta - offset) / maxAngle) * windowWidth;
     }
